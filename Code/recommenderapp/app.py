7dbcbabcb81e3243c9537686bfd8896415bd4065
Atharva Thorve <aaathorve@gmail.com>
2023-10-13 22:35:39 -0400

    Templating added

--- Code/recommenderapp/app.py
+++ Code/recommenderapp/app.py
@@ -61,7 +61,7 @@ def feedback():
 @app.route("/comments/<movie>")
 def comments(movie):
     
-    return render_template("view_comments.html")
+    return render_template("view_comments.html", movie=movie)
 
 
 @app.route("/success")
