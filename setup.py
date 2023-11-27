579890578470fdd26318af3442e5ba2c32795f1a
Nihit Mittal <nihitmittal@Nihits-MBP.lan>
2023-11-27 00:31:34 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="raghavnarula",  # Replace with your own username
+    name="nihitmittal",  # Replace with your own username
     version="1.0.0",
-    author="Atharva, Divit, Mrityunjay, Raghav",
-    author_email="aaathorve@gmail.com",
+    author="Aryaman Jain, Ankit Garg, Nihit Mittal",
+    author_email="nmittal2@ncsu.edu",
     description="A recommendation engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/raghavnarula/MovieRecommender",
+    url="https://github.com/nihitmittal/MovieRecommender.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
